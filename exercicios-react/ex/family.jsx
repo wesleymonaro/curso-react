bbce831a33638e08ff807785ba6b630a9fc8429b
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-05 23:31:41 -0200

    Classe Util

--- exercicios-react/ex/family.jsx
+++ exercicios-react/ex/family.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import { childrenWithProps } from '../utils/reactUtils'
 
 export default props => (
     <div>
         <h1>Família</h1>
-        {props.children}
-        { React.cloneElement(props.children, {...props}) }
+        { childrenWithProps(props.children, props) }
     </div>
 )
