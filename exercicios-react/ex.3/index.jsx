c7dbe41daf9b53aac8f019832ded0faadd3c429b
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-05 23:04:33 -0200

    Exportando mais de um componente

---
+++ exercicios-react/ex.3/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Component from './componente'
+
+ReactDOM.render(<Component value='Show!' />, document.getElementById('app'))
--- exercicios-react/ex/componente.jsx
+++ exercicios-react/ex/componente.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
-export default props => (
-    <h1>{props.value}</h1>
+const Primeiro = props => <h1>Primeiro Componente</h1>
+
+
+const Segundo = props => (
+    <h1>Segundo Componente</h1>
 )
+
+export { Primeiro, Segundo }
